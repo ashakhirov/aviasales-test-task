6e16fd1c2faadb9bdd2238d4dd597311331f1c61
shahirov <ara.shahirov@gmail.com>
2020-07-11 22:00:12 +0500

    Create helper function for http request

---
+++ src/lib/request/index.ts
@@ -0,0 +1,25 @@
+type Method = 'get'
+
+type RequestAttr = {
+  method: Method
+  path: string
+  params?: Record<string, string>
+}
+
+type Request = <T>({ method, path, params }: RequestAttr) => Promise<T>
+
+/**
+ * make request
+ */
+export const request: Request = async ({ method, path, params }) => {
+  const options: RequestInit = { method: method.toUpperCase() }
+  const url = new URL(`${process.env.API_URL}/${path}`)
+
+  if (params) {
+    url.search = new URLSearchParams(params).toString()
+  }
+
+  const response = await fetch(url.href, options)
+
+  return response.json()
+}
