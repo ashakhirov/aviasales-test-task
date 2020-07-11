6315440f5e531bf84ae8e5d8a75ff6563abffb0b
shahirov <ara.shahirov@gmail.com>
2020-07-11 22:00:42 +0500

    Create Skeleton ui component

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export { AppBar } from './app-bar'
 export { Layout } from './layout'
 export { Main } from './main'
+export { Skeleton } from './skeleton'
---
+++ src/ui/skeleton/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+export const Skeleton: React.FC = () => {
+  return (
+    <Box>
+      <Wrapper>
+        <LinePlaceholder width="80" height="13px" mb="10px" />
+        <LinePlaceholder width="40%" height="9px" />
+      </Wrapper>
+      <Wrapper>
+        <Segment>
+          <LinePlaceholder width="100%" height="11px" mb="5px" />
+          <LinePlaceholder width="75%" height="9px" />
+        </Segment>
+        <Segment>
+          <LinePlaceholder width="100%" height="11px" mb="5px" />
+          <LinePlaceholder width="75%" height="9px" />
+        </Segment>
+      </Wrapper>
+    </Box>
+  )
+}
+
+const Box = styled.div`
+  margin-bottom: 20px;
+  padding: 20px;
+  width: 100%;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  background-color: #ffffff;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const Segment = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`
+
+const loading = keyframes`
+  100% {
+      transform: translateX(100%);
+    }
+`
+
+type PlaceholderProps = {
+  width: string
+  height: string
+  mt?: string
+  mr?: string
+  mb?: string
+  ml?: string
+}
+
+const LinePlaceholder = styled.div<PlaceholderProps>`
+  position: relative;
+  margin-bottom: ${({ mb }) => mb && mb};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  overflow: hidden;
+  background-color: #e2e2e2;
+
+  &::after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.2),
+      transparent
+    );
+
+    animation: ${loading} 0.7s infinite;
+  }
+`
