4ef5ef44560adffa3004a8b0b89c13342a6a7273
shahirov <ara.shahirov@gmail.com>
2020-07-13 00:52:38 +0500

    Add functions to format time

---
+++ src/lib/date/index.ts
@@ -0,0 +1,47 @@
+/**
+ *  format duration from minutes to hh:mm
+ * @param {number} timeInMin
+ * @returns {string} formated string hh:mm
+ */
+export const formatDuration = (timeInMin = 0): string => {
+  const hh = Math.floor(timeInMin / 60)
+  const mm = timeInMin === 0 ? timeInMin : timeInMin - hh * 60
+
+  return `${hh}ч ${mm}м`
+}
+
+/**
+ * get time from date in string format
+ * @param {number} date date that needs format
+ * @param {string} timezone timezone
+ * @returns {string} time in string format
+ */
+export const getTimeFromDate = (date: number, timezone = 'UTC'): string => {
+  const options = {
+    timezone,
+    hour: 'numeric',
+    minute: 'numeric',
+  }
+
+  return new Date(date).toLocaleTimeString('ru', options)
+}
+
+/**
+ * format time to `startTime - endTime`
+ * @param {Date} date initial date
+ * @param {number} duration duration in minutes
+ * @param {string} timezone timezone
+ * @returns {string} `startTime - endTime`
+ */
+export const formatTimeInterval = (
+  date: Date,
+  duration: number,
+  timezone: string,
+): string => {
+  const startTime = new Date(date).getTime()
+  const endTime = startTime + 60 * 1000 * duration
+  const formattedStartTime = getTimeFromDate(startTime, timezone)
+  const formattedEndTime = getTimeFromDate(endTime, timezone)
+
+  return `${formattedStartTime} – ${formattedEndTime}`
+}
