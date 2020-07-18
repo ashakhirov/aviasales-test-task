2891d77d475ecc9e2760c82fcde020d6e5fb25ef
shahirov <ara.shahirov@gmail.com>
2020-07-18 21:33:50 +0500

    Add effecor-logger and effector-inspector packages, update .babelrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { createInspector } from 'effector-inspector'
 
 import { fetchTicketsStart } from 'features/tickets'
 import { store } from './app/store'
 import App from './app'
+
+createInspector()
 
 store.dispatch(fetchTicketsStart())
 
